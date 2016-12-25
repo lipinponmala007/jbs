08990426c7257a9f6124ce0a6515d8c335ad71d5
sbalaga <sbalaga@ikriti.com>
2016-12-26 03:57:09 +0530

    job list styled

--- sites/all/themes/jbs/templates/node/node--jobs--teaser.tpl.php
+++ sites/all/themes/jbs/templates/node/node--jobs--teaser.tpl.php
@@ -1,2 +1,11 @@
-<?php 
-?>
+<div class="panel panel-list-item">
+	<div class="panel-body">
+		<h3><a href="<?php print $variables['node_url']; ?>"><?php print $variables['title']; ?></a></h3>
+
+		<?php print render($variables['content']['body']); ?>
+		<?php print render($variables['content']['links']); ?>
+	</div>
+	<div class="panel-heading">
+		<span class="pull-right"><?php print ($variables['date']); ?></span>
+	</div>
+</div>
