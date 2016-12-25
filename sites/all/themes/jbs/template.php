ba79d55471b6d98b0393f15dd2cfc47a66c31179
lponmala <lponmala@inkriti.com>
2016-12-26 03:12:58 +0530

    teaser templetes

--- sites/all/themes/jbs/template.php
+++ sites/all/themes/jbs/template.php
@@ -394,6 +394,8 @@ function jbs_preprocess_node(&$variables) {
       $function($variables);
     }
   }
+  
+  $variables['theme_hook_suggestions'][] = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
 }
 
 
