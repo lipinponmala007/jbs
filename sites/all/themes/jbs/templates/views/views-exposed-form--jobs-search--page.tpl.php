9047921fb9b9373e6359a4e0386f241f16af623f
sbalaga <sbalaga@ikriti.com>
2016-12-26 02:00:57 +0530

    removed messages

--- sites/all/themes/jbs/templates/views/views-exposed-form--jobs-search--page.tpl.php
+++ sites/all/themes/jbs/templates/views/views-exposed-form--jobs-search--page.tpl.php
@@ -7,19 +7,15 @@
 	<h3 class="hdg-3"><?php print t('Search jobs') ?></h3>
 	<div class="row form-wrap">
 		<div class="col-lg-4">
-		  <?php print $variables['form']['#info']['filter-field_work_location_tid']['label'] ;?>
 			<?php print render($variables['form']['field_work_location_tid'])?>
 		</div>
 		<div class="col-lg-3">
-		<?php print $variables['form']['#info']['filter-field_skills_tid']['label'] ;?>
 		  <?php print render($variables['form']['field_skills_tid']);?>
 		</div>
 		<div class="col-lg-2">
-		<?php print $variables['form']['#info']['filter-field_company_tid']['label'] ;?>
 			<?php print render($variables['form']['field_company_tid']);?>
 		</div>
 		<div class="col-lg-2">
-		<?php print$variables['form']['#info']['filter-field_occupational_field_tid']['label'] ;?>
 			<?php print render($variables['form']['field_occupational_field_tid']);?>
 		</div>
 		<div class="col-lg-1">
